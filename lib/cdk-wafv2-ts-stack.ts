b5db288cdb703ca84c8e1ab898ec068d8dc7e673
EC2 Default User <ec2-user@ip-172-31-26-130.us-east-2.compute.internal>
2021-03-25 02:39:26 +0000

    first commit

--- lib/cdk-wafv2-ts-stack.ts
+++ lib/cdk-wafv2-ts-stack.ts
@@ -1,9 +1,42 @@
 import * as cdk from '@aws-cdk/core';
 
+import { CfnWebACL } from '@aws-cdk/aws-wafv2';
+
 export class CdkWafv2TsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const basicRule: CfnWebACL.RuleProperty = {
+            name: 'AWS-AWSManagedRulesCommonRuleSet',
+            priority: 0,
+            statement: {
+                managedRuleGroupStatement: {
+                    vendorName: 'AWS',
+                    name: 'AWSManagedRulesCommonRuleSet',
+                },
+            },
+            overrideAction: {
+                count: {},
+            },
+            visibilityConfig: {
+                cloudWatchMetricsEnabled: true,
+                sampledRequestsEnabled: true,
+                metricName: 'AWS-AWSManagedRulesCommonRuleSet',
+            },
+        };
+
+        const webACL: CfnWebACL = new CfnWebACL(this, 'WebAcl', {
+            defaultAction: {
+                allow: {},
+            },
+            scope: 'REGIONAL',
+            visibilityConfig: {
+                cloudWatchMetricsEnabled: true,
+                metricName: "test-waf-metric",
+                sampledRequestsEnabled: true,
+            },
+            rules: [basicRule],
+        });
   }
 }
